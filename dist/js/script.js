d911ccf71da6580fa5bea6994626701dc1e278d2
Dinara <dinara.baqyt@gmail.com>
2023-04-17 16:40:18 +0600

    zero

--- dist/js/script.js
+++ dist/js/script.js
@@ -42,14 +42,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // Timer
 
-        const deadline = '2023-04-25';
+        const deadline = '2023-03-25';
 
         function getTimeRemaining(endTime) {
-          const t = Date.parse(endTime) - Date.parse(new Date()),
+          let days, hours, minutes, seconds;
+          const t = Date.parse(endTime) - Date.parse(new Date());
+
+          if(t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+          } else {
                 days = Math.floor(t / (1000 * 60 * 60 * 24)),
                 hours = Math.floor((t / (1000 * 60 * 60) % 24)),
                 minutes = Math.floor((t/ 1000 / 60) % 60),
                 seconds = Math.floor((t/ 1000 % 60));
+          };
+                
 
           return {
             'total': t,
